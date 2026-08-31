09df349e8cd5c4f8afc1095fd35de16f898f5dfe
StochasticEagle <179076459+StochasticEagle@users.noreply.github.com>
2026-08-30 22:31:52 -0600

    Converted fully to use git submodules.

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,55 +1,60 @@
 #!/bin/bash
 
-## Fail script if an error happens
 set -e
 
-## Enter the pspsdk directory.
-cd "$(dirname "$0")" || { echo "ERROR: Could not enter the pspsdk directory."; exit 1; }
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-## Make sure PSPDEV is set
-if [ -z "${PSPDEV}" ]; then
-    echo "The PSPDEV environment variable has not been set"
+## Make sure PSPDEV is set.
+if [ -z "${PSPDEV:-}" ]; then
+    echo "ERROR: The PSPDEV environment variable has not been set."
     exit 1
 fi
 
-## Build pspsdk
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
+
+## Build PSPSDK.
+cd "${ROOT}"
 ./bootstrap
 ./configure
-make clean
-make -j $(getconf _NPROCESSORS_ONLN)
+make -j "$PROC_NR"
 
-## Install pspsdk
-make install -j $(getconf _NPROCESSORS_ONLN)
+## Install PSPSDK.
+make -j "$PROC_NR" install
 
-## gcc needs to include libcglue libpthreadglue libpspprof libpsputility libpsprtc libpspnet_inet libpspnet_resolver libpspsdk libpspmodinfo libpspuser libpspkernel
-## from pspsdk to be able to build executables, because they are part of the standard libraries
-cd "$PSPDEV/psp/lib"
-ln -sf "../sdk/lib/libcglue.a" "libcglue.a" || { exit 1; }
-ln -sf "../sdk/lib/libpthreadglue.a" "libpthreadglue.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspprof.a" "libpspprof.a" || { exit 1; }
-ln -sf "../sdk/lib/libpsputility.a" "libpsputility.a" || { exit 1; }
-ln -sf "../sdk/lib/libpsprtc.a" "libpsprtc.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspnet_inet.a" "libpspnet_inet.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspnet_resolver.a" "libpspnet_resolver.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspsdk.a" "libpspsdk.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspmodinfo.a" "libpspmodinfo.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspuser.a" "libpspuser.a" || { exit 1; }
-ln -sf "../sdk/lib/libpspkernel.a" "libpspkernel.a" || { exit 1; }
-cd -
+## GCC needs to include libcglue, libpthreadglue, libpspprof,
+#  libpsputility, libpsprtc, libpspnet_inet, libpspnet_resolver,
+#  libpspsdk, libpspmodinfo, libpspuser, and libpspkernel from
+#  PSPSDK to be able to build executables because they are part
+#  of the standard libraries.
 
-# Copy licenses
-mkdir -p $PSPDEV/psp/share/licenses/pspsdk
-cp LICENSE $PSPDEV/psp/share/licenses/pspsdk/
+LIBDIR="${PSPDEV}/psp/lib"
 
-mkdir -p $PSPDEV/share/licenses/PrxEncrypter
-cp tools/PrxEncrypter/LICENSE $PSPDEV/share/licenses/PrxEncrypter
+ln -sf "../sdk/lib/libcglue.a" "${LIBDIR}/libcglue.a"
+ln -sf "../sdk/lib/libpthreadglue.a" "${LIBDIR}/libpthreadglue.a"
+ln -sf "../sdk/lib/libpspprof.a" "${LIBDIR}/libpspprof.a"
+ln -sf "../sdk/lib/libpsputility.a" "${LIBDIR}/libpsputility.a"
+ln -sf "../sdk/lib/libpsprtc.a" "${LIBDIR}/libpsprtc.a"
+ln -sf "../sdk/lib/libpspnet_inet.a" "${LIBDIR}/libpspnet_inet.a"
+ln -sf "../sdk/lib/libpspnet_resolver.a" "${LIBDIR}/libpspnet_resolver.a"
+ln -sf "../sdk/lib/libpspsdk.a" "${LIBDIR}/libpspsdk.a"
+ln -sf "../sdk/lib/libpspmodinfo.a" "${LIBDIR}/libpspmodinfo.a"
+ln -sf "../sdk/lib/libpspuser.a" "${LIBDIR}/libpspuser.a"
+ln -sf "../sdk/lib/libpspkernel.a" "${LIBDIR}/libpspkernel.a"
 
-## Store build information
-if [ -d .git ]; then
-  BUILD_FILE="${PSPDEV}/build.txt"
-  if [[ -f "${BUILD_FILE}" ]]; then
-    sed -i'' '/^pspsdk /d' "${BUILD_FILE}"
-  fi
-  git log -1 --format="pspsdk %H %cs %s" >> "${BUILD_FILE}"
+## Copy licenses.
+mkdir -p "${PSPDEV}/psp/share/licenses/pspsdk"
+cp "${ROOT}/LICENSE" "${PSPDEV}/psp/share/licenses/pspsdk/"
+
+mkdir -p "${PSPDEV}/share/licenses/PrxEncrypter"
+cp "${ROOT}/tools/PrxEncrypter/LICENSE" "${PSPDEV}/share/licenses/PrxEncrypter/"
+
+## Store build information.
+if git -C "${ROOT}" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    BUILD_FILE="${PSPDEV}/build.txt"
+
+    if [[ -f "${BUILD_FILE}" ]]; then
+        sed -i'' '/^pspsdk /d' "${BUILD_FILE}"
+    fi
+
+    git -C "${ROOT}" log -1 --format="pspsdk %H %cs %s" >> "${BUILD_FILE}"
 fi
-
